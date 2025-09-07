5fba4e3074bfb5ccbe369d14a5d5ded03bf5a2dd
C4AR-HENRI <carolina4aleixo@gmail.com>
2025-09-07 14:51:53 +0100

    integer to roman exercise, link to exercise in file line 1

---
+++ Integer to roman.py
@@ -0,0 +1,12 @@
+## https://leetcode.com/problems/integer-to-roman/description/
+
+roman_no = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+indian_no = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+
+num = int(input("Enter a no between 1 and 3999"))
+roman_num = None
+
+while num > 0:
+
+
+
